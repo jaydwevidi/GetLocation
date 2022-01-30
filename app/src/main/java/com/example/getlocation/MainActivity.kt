d8185620214e3e8dda2c3338c143115cdf09604b
Jay Dwived <jayraj1999@gmail.com>
2022-01-30 11:50:22 +0530

    location updates

--- app/src/main/java/com/example/getlocation/MainActivity.kt
+++ app/src/main/java/com/example/getlocation/MainActivity.kt
@@ -5,32 +5,101 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
 import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest : LocationRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        locationRequest = LocationRequest.create()
+        locationRequest.setInterval(2000)
+        locationRequest.setFastestInterval(1000)
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+
+
         getLocationFine()
+        checkSettingsThenStart()
 
         findViewById<Button>(R.id.button1).setOnClickListener {
-            mapActivity()
+            startMapActivity()
         }
 
+    }
 
+
+    private fun checkSettingsThenStart(){
+        val request = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest).build()
+
+        val client = LocationServices.getSettingsClient(this);
+
+        val task : Task<LocationSettingsResponse> = client.checkLocationSettings(request)
+
+        val successListener = object : OnSuccessListener<LocationSettingsResponse> {
+            override fun onSuccess(p0: LocationSettingsResponse?) {
+                startLocationUpdates()
+            }
+
+        }
+
+        val failureListener = object : OnFailureListener {
+            override fun onFailure(p0: Exception) {
+                Log.e("Failed", "onFailureListener: " )
+            }
+
+        }
+
+        task.addOnSuccessListener (successListener)
+        task.addOnFailureListener (failureListener)
+
+    }
+
+    val locationCallback = object : LocationCallback(){
+
+        override fun onLocationResult(locationResult: LocationResult) {
+            for (i in locationResult.locations){
+                Log.d("TAG", "onLocationResult: ${i.latitude} , ${i.longitude}")
+            }
+            super.onLocationResult(locationResult)
+        }
+    }
+
+    private fun startLocationUpdates(){
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Toast.makeText(this, "updates Location no permission", Toast.LENGTH_SHORT).show()
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest , locationCallback , Looper.getMainLooper())
+    }
+
+    private fun stopLocationUpdates(){
+        fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
     private fun checkLocationFinePermission() : Boolean {
@@ -65,14 +134,13 @@ class MainActivity : AppCompatActivity() {
             .check()
     }
 
-    private fun mapActivity(){
+    private fun startMapActivity(){
         val i = Intent(this , MapsActivity::class.java)
         startActivity(i)
     }
 
     fun getLocationFine(){
         //Toast.makeText(this, "get Fine called", Toast.LENGTH_SHORT).show()
-
 
         if (ActivityCompat.checkSelfPermission(
                 this,
